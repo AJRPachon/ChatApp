3d7f433f2ff0f6914e4d6b28d91c4ba408d349a5
AJRPachon <ajrpachon@gmail.com>
2026-06-11 17:55:05 +0200

    Add profile-screen

---
+++ app/src/main/java/com/ajrpachon/chatapp/ui/profile/ProfileContract.kt
@@ -0,0 +1,14 @@
+package com.ajrpachon.chatapp.ui.profile
+
+data class ProfileState(
+    val displayName: String = "",
+    val username: String = "",
+    val email: String = "",
+    val avatarUrl: String? = null,
+    val isUploadingAvatar: Boolean = false,
+    val error: String? = null,
+)
+
+sealed interface ProfileEffect {
+    data object NavigateToAuth : ProfileEffect
+}
